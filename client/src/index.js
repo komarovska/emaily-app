3e09636f417fc1460d38282f363332eafe02a00f
viktoriakomarovskaya <viktoriia.komarovska@sixt.com>
2020-05-09 23:59:13 +0300

    adding data to mongo

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter, Router } from 'react-router-dom';
+import { RouterStore, syncHistoryWithStore } from 'mobx-react-router';
+import App from './components/App';
+import ApplicationStore from './stores/application';
+
+const routingStore = new RouterStore();
+const history = syncHistoryWithStore(createBrowserHistory(), routingStore);
+
+const stores = {
+    routing: routingStore,
+    application: new ApplicationStore(),
+};
+
+ReactDOM.render(
+    <Provider { ...stores }>
+        <BrowserRouter basename="/">
+            <Router history={history}>
+                <App />
+            </Router>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);

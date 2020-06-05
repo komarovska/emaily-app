e6c894e1a17a929f4b7ec8f685d6dfcf9989ccf1
Viktoriia Komarovska <viktoriia@MacBook-Pro-Viktoriia.local>
2020-06-06 00:01:41 +0300

    add routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
     render () {
-        return <div>MOVIE PLACE</div>
+        return (
+            <div>
+        <div>MOVIE PLACE</div>
+        <NavLink to="/movies">movies list</NavLink>
+        <NavLink to="/form">submit new movie</NavLink>
+        </div>
+        )
     }
 }
 
